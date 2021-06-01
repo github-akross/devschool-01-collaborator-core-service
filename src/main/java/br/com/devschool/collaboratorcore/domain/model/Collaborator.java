b9b0e00d2d0140cc58c95a7c8fa49c24f7ef2f26
Thales Macena <thalesmacena@gmail.com>
2021-06-01 12:25:18 -0300

    Criação da estrutura básica do projeto

---
+++ src/main/java/br/com/devschool/collaboratorcore/domain/model/Collaborator.java
@@ -0,0 +1,4 @@
+package br.com.devschool.collaboratorcore.domain.model;
+
+public class Collaborator {
+}
---
+++ src/main/java/br/com/devschool/collaboratorcore/infrastructure/repository/CollaboratorRepository.java
@@ -0,0 +1,4 @@
+package br.com.devschool.collaboratorcore.infrastructure.repository;
+
+public class CollaboratorRepository {
+}
