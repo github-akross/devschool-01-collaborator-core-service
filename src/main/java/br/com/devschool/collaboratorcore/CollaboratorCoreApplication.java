ba80928925cbf54488e19e86b0f421dffc9b10d6
Sam Sepiol <igordmc92@gmail.com>
2021-06-01 10:57:26 -0300

    Criação do projeto Spring

---
+++ src/main/java/br/com/devschool/collaboratorcore/CollaboratorCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.devschool.collaboratorcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaboratorCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaboratorCoreApplication.class, args);
+	}
+
+}
